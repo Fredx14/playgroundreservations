4f3a137b0500846e46f284fb14f705ad9331f113
Mohamed <mohamed@Mohameds-MacBook-Pro.local>
2020-12-04 17:54:56 +0200

    Add logout

--- app/src/main/java/com/example/booking/MainActivity.java
+++ app/src/main/java/com/example/booking/MainActivity.java
@@ -7,7 +7,9 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -94,5 +96,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void openReservationList() {
         Intent intent = new Intent(this, ReservationList.class);
         startActivity(intent);
+    }
+
+    public void logout(View v){
+        // Delete all SharedPref
+
+        /*  SharedPreferences  */
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.remove("user");
+        editor.clear();
+        editor.commit();
+        editor.apply();
+        /*  ./SharedPreferences  */
+
+        // Redirect to login page
+        Intent intent = new Intent(getApplicationContext(),Login.class);
+        startActivity(intent);
+
     }
 }
